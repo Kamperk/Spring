c7a87ed339814f2b01b72720619f8dd249cffb0f
kapustinmaksim <kapustin-19-08@mail.ru>
2021-03-22 17:39:45 +0300

    Solution

--- src/main/java/koschei/models/Ocean1.java
+++ src/main/java/koschei/models/Ocean1.java
@@ -6,11 +6,18 @@ import org.springframework.stereotype.Component;
 @Component
 public class Ocean1 {
 
-    @Autowired
     private Island2 island;
+
+    @Autowired
+    public Ocean1(Island2 island){
+        this.island = island;
+    }
 
     @Override
     public String toString() {
         return "на океане остров " + island.toString();
     }
+
+
 }
+
